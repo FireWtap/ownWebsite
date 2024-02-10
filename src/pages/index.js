aaf915fd69dee6c1852deb799fae2d3fc418eda2
Francesco Massafra <massafra32@gmail.com>
2024-02-10 16:10:43 +0100

    added speedinsights vercel

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
@@ -14,6 +15,7 @@ const IndexPage = ({ location }) => (
       <About />
       <Jobs />
       <Contact />
+      <SpeedInsights />
     </StyledMainContainer>
   </Layout>
 );
