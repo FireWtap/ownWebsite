d011a7af71a19e4a65b7d707c6e8eb62e2d3a784
francesco <massafra32@gmail.com>
2024-02-12 20:48:04 +0100

    fixs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
+import { Layout, Hero, About, Jobs, Featured, Projects, Contact, Studies } from '@components';
 import { SpeedInsights } from '@vercel/speed-insights/react';
-import Studies from '../components/sections/studies';
 const StyledMainContainer = styled.main`
   counter-reset: section;
 `;
